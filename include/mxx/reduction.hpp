166c6d37b8a1649a79b4d03d5fca2adeba18e3b0
r4d2 <patrick.flick@gmail.com>
2015-06-05 23:14:14 -0400

    custom reduction operators and tests

--- include/mxx/reduction.hpp
+++ include/mxx/reduction.hpp
@@ -21,6 +21,7 @@
 
 // mxx includes
 #include "datatypes.hpp"
+#include "comm.hpp"
 
 namespace mxx {
 
@@ -84,55 +85,142 @@ public:
  *                     User supplied functions:                      *
  *********************************************************************/
 
-// Wraps custom operators. requires that the MPI_Datatype be duplicated
-// since it attaches the operator as a std::function to the MPI_Datatype
-// object.
+// an Op is not built-in in general:
+template <typename T, typename Func>
+struct get_builtin_op {
+    static MPI_Op op(Func) {
+        return MPI_OP_NULL;
+    }
+};
+
+// define all C++ functors that map to a MPI builtin MPI_Op to directly map
+// to that builtin MPI_Op
+#define MXX_BUILTIN_OP(mpi_op, cpp_functor)                                    \
+template <typename T>                                                          \
+struct get_builtin_op<T, cpp_functor<T> > {                                      \
+    static MPI_Op op(cpp_functor<T>) {                                           \
+        return mpi_op;                                                        \
+    }                                                                          \
+};                                                                             \
+
+MXX_BUILTIN_OP(MPI_SUM, std::plus);
+MXX_BUILTIN_OP(MPI_PROD, std::multiplies);
+MXX_BUILTIN_OP(MPI_LAND, std::logical_and);
+MXX_BUILTIN_OP(MPI_LOR, std::logical_or);
+MXX_BUILTIN_OP(MPI_BOR, std::bit_or);
+MXX_BUILTIN_OP(MPI_BXOR, std::bit_xor);
+MXX_BUILTIN_OP(MPI_BAND, std::bit_and);
+
+#undef MXX_BUILTIN_OP
+
+
+// for std::min/std::max functions
+// TODO: this doesn't find the type if it is passed as Func& func in custom_op constructor
+template <typename T>
+struct get_builtin_op<T, const T&(*) (const T&, const T&)> {
+    static MPI_Op op(const T& (*t)(const T&, const T&)) {
+        // check if function is std::min or std::max
+        if (t == static_cast<const T&(*)(const T&, const T&)>(std::min<T>)){
+            return MPI_MIN;
+        } else if (t == static_cast<const T&(*)(const T&, const T&)>(std::max<T>)){
+            return MPI_MAX;
+        } else {
+            // otherwise return NULL
+            return MPI_OP_NULL;
+        }
+    }
+};
+
+
+/**
+ * @brief   Wrapps a binary combination/reduction operator for MPI use in
+ *          custom operators.
+ *
+ * @note    This assumes that the operator is commutative.
+ *
+ * @tparam T    The input and ouput datatype of the binary operator.
+ * @tparam IsCommutative    Whether or not the operation is commutative (default = true).
+ */
 template <typename T, bool IsCommutative = true>
 class custom_op {
 public:
-    template <typename Func>
-    custom_op(Func& func, const MPI_Datatype& type) : builtin(false) {
-        // create user function
-        typedef std::function<void(void*,void*,int*)> func_t;
-        func_t user_func = [&func](void* invec, void* inoutvec, int* len) {
-            T* in = (T*) invec;
-            T* inout = (T*) inoutvec;
-            for (int i = 0; i < *len; ++i) {
-                inout[i] = func(in[i], inout[i]);
-            }
-        };
-        // attach function to the datatype
-        MPI_Type_dup(type, &type_copy);
-        attr_map<int, func_t>::set(type_copy, 1347, user_func);
-
-        // create op
-        MPI_Op_create(&custom_op::user_function, IsCommutative, &op);
-    }
-
-    /* TODO: could potentially overload like the following for builtin
-     *       functions and types. this requires checking for both: builtin type
-     *       and builtin function
-     *
-    template <typename S>
-    custom_op(std::plus<S>&, const MPI_Datatype& type)
-        : builtin(true)
-    {
-        std::cout << "use builtin function!" << std::endl;
-        type_copy = type;
-        op = MPI_SUM;
-    }
-    */
 
     /**
-     * @brief   Returns the MPI_Datatype which contains the std::function
-     *          reference.
+     * @brief Creates a custom operator given a functor and the associated
+     *        `MPI_Datatype`.
+     *
+     * @tparam Func     Type of the functor, can be a function pointer, lambda
+     *                  function, or std::function or any object with a
+     *                  `T operator(T& x, T& y)` member.
+     * @param func      The instance of the functor.
+     */
+    template <typename Func>
+    custom_op(Func& func) : builtin(false) {
+        if (mxx::is_builtin_type<T>::value) {
+            // check if the operator is MPI built-in (in case the type
+            // is also a MPI built-in type)
+            // TODO: somehow having `func` as a reference parameter
+            //       vs non reference parameter messes up different
+            //       test cases. what's going on??
+            // TODO: quetsion is: how are function pointers passed when
+            //       the input parameter is a reference of templated type???
+            MPI_Op o = get_builtin_op<T, Func>::op(func);
+            if (o != MPI_OP_NULL) {
+                // this op is builtin, save it as such and don't copy built-in type
+                builtin = true;
+                op = o;
+                mxx::datatype<T> dt;
+                type_copy = dt.type();
+            }
+        }
+        if (!builtin) {
+            // create user function
+            typedef std::function<void(void*,void*,int*)> func_t;
+            func_t user_func = [&func](void* invec, void* inoutvec, int* len) {
+                T* in = (T*) invec;
+                T* inout = (T*) inoutvec;
+                for (int i = 0; i < *len; ++i) {
+                    std::cout << "i=" << i << ", len = " << *len << ", in[i]=" << in[i] << ", inout[i]=" << inout[i] << std::endl;
+                    std::cout << "&f=" << &func << std::endl;
+                    //std::cout << "f=" << func << std::endl;
+                    inout[i] = func(in[i], inout[i]);
+                }
+            };
+            // get datatype associated with the type `T`
+            mxx::datatype<T> dt;
+            // attach function to a copy of the datatype
+            MPI_Type_dup(dt.type(), &type_copy);
+            attr_map<int, func_t>::set(type_copy, 1347, user_func);
+            // create op
+            MPI_Op_create(&custom_op::user_function, IsCommutative, &op);
+        }
+    }
+
+    /**
+     * @brief   Returns the MPI_Datatype which has to be used in conjuction
+     *          with the MPI_Op operator.
+     *
+     * The custom operators are wrapped into `std::function` objects and
+     * saved/attached to a duplicated MPI_Datatype as MPI attribute.
+     *
+     * When MPI calls the custom user function, the MPI_Datatype is supplied
+     * and thus the `std::function` object can be accessed and executed.
+     *
+     * @returns The `MPI_Datatype` which has to be used in conjuction with the
+     *          `MPI_Op` returned by `get_op()` for all MPI reduction operations.
      */
     MPI_Datatype get_type() const {
         return type_copy;
     }
 
     /**
-     * @brief   Returns the MPI_Op operator for reduction operations.
+     * @brief   Returns the `MPI_Op` operator for reduction operations.
+     *
+     * @note
+     * The MPI operator `MPI_Op` returned by this function can only be used in
+     * conjunction with the `MPI_Datatype` returned by `get_type()`.
+     *
+     * @returns     The MPI operator as `MPI_Op` object.
      */
     MPI_Op get_op() const {
         return op;
@@ -141,6 +229,7 @@ public:
     /// Destructor: cleanup MPI objects
     virtual ~custom_op() {
         if (!builtin) {
+            // clean-up (only if this wasn't a built-in MPI_Op)
             MPI_Op_free(&op);
             MPI_Type_free(&type_copy);
         }
@@ -172,85 +261,67 @@ private:
 // TODO: template specialize for std::min, std::max, std::plus, std::multiply
 //       etc for integers to use MPI builtin ops?
 
-template <typename T>
-T allreduce(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get type
-    mxx::datatype<T> dt;
-    T result;
-    MPI_Allreduce(&x, &result, 1, dt.type(), MPI_SUM, comm);
-    return result;
-}
-
-template <typename T>
-T reduce(T& x, MPI_Comm comm = MPI_COMM_WORLD, int root = 0)
-{
-    // get type
-    mxx::datatype<T> dt;
-    T result;
-    MPI_Reduce(&x, &result, 1, dt.type(), MPI_SUM, root, comm);
-    return result;
-}
 
 template <typename T, typename Func>
-T allreduce(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get type
-    mxx::datatype<T> dt;
+T reduce(const T& x, int root, Func func, const mxx::comm& comm = mxx::comm()) {
     // get custom op (and type for custom op)
-    mxx::custom_op<T> op(func, dt.type());
-    // perform reduction
-    T result;
-    MPI_Allreduce(&x, &result, 1, op.get_type(), op.get_op(), comm);
-    // return result
+    mxx::custom_op<T> op(func);
+    T result = T();
+    MPI_Reduce(const_cast<T*>(&x), &result, 1, op.get_type(), op.get_op(), root, comm);
     return result;
 }
 
 template <typename T>
-T exscan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get type
-    mxx::datatype<T> dt;
+T reduce(const T& x, int root, const mxx::comm& comm = mxx::comm()) {
+    return reduce(x, root, std::plus<T>(), comm);
+}
+
+// TODO: vectorized ops
+
+template <typename T, typename Func>
+T allreduce(const T& x, Func func, const mxx::comm& comm = mxx::comm()) {
+    // get custom op (and type for custom op)
+    mxx::custom_op<T> op(func);
+    // perform reduction
     T result;
-    MPI_Exscan(&x, &result, 1, dt.type(), MPI_SUM, comm);
-    int rank;
-    MPI_Comm_rank(comm, &rank);
-    if (rank == 0)
-      result = T();
+    MPI_Allreduce(const_cast<T*>(&x), &result, 1, op.get_type(), op.get_op(), comm);
     return result;
 }
 
+template <typename T>
+T allreduce(const T& x, const mxx::comm& comm = mxx::comm()) {
+    return allreduce(x, std::plus<T>(), comm);
+}
+
 template <typename T, typename Func>
-T exscan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get type
-    mxx::datatype<T> dt;
+T scan(const T& x, Func func, const mxx::comm& comm = mxx::comm()) {
     // get op
-    mxx::custom_op<T> op(func, dt.type());
+    mxx::custom_op<T> op(func);
+    T result;
+    MPI_Scan(const_cast<T*>(&x), &result, 1, op.get_type(), op.get_op(), comm);
+    return result;
+}
+template <typename T>
+T scan(const T& x, const mxx::comm& comm = mxx::comm()) {
+    return scan(x, std::plus<T>(), comm);
+}
+
+
+template <typename T, typename Func>
+T exscan(const T& x, Func func, const mxx::comm& comm = mxx::comm()) {
+    // get op
+    mxx::custom_op<T> op(func);
     // perform reduction
     T result;
-    MPI_Exscan(&x, &result, 1, op.get_type(), op.get_op(), comm);
-    int rank;
-    MPI_Comm_rank(comm, &rank);
-    if (rank == 0)
+    MPI_Exscan(const_cast<T*>(&x), &result, 1, op.get_type(), op.get_op(), comm);
+    if (comm.rank() == 0)
       result = T();
     return result;
 }
 
 template <typename T>
-T scan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get type
-    mxx::datatype<T> dt;
-    T result;
-    MPI_Scan(&x, &result, 1, dt.type(), MPI_SUM, comm);
-    return result;
-}
-
-template <typename T, typename Func>
-T scan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get type
-    mxx::datatype<T> dt;
-    // get op
-    mxx::custom_op<T> op(func, dt.type());
-    T result;
-    MPI_Scan(&x, &result, 1, op.get_type(), op.get_op(), comm);
-    return result;
+T exscan(const T& x, const mxx::comm& comm = mxx::comm()) {
+    return exscan(x, std::plus<T>(), comm);
 }
 
 /****************************************************
@@ -268,7 +339,7 @@ inline void rev_comm(MPI_Comm comm, MPI_Comm& rev)
 }
 
 template <typename T>
-T reverse_exscan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
+T reverse_exscan(T& x, const mxx::comm& comm = mxx::comm()) {
     MPI_Comm rev;
     rev_comm(comm, rev);
     T result = exscan(x, rev);
@@ -277,7 +348,7 @@ T reverse_exscan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
 }
 
 template <typename T, typename Func>
-T reverse_exscan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
+T reverse_exscan(T& x, Func func, const mxx::comm& comm = mxx::comm()) {
     MPI_Comm rev;
     rev_comm(comm, rev);
     T result = exscan(x, func, rev);
@@ -286,7 +357,7 @@ T reverse_exscan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
 }
 
 template <typename T>
-T reverse_scan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
+T reverse_scan(T& x, const mxx::comm& comm = mxx::comm()) {
     MPI_Comm rev;
     rev_comm(comm, rev);
     T result = scan(x, rev);
@@ -295,7 +366,7 @@ T reverse_scan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
 }
 
 template <typename T, typename Func>
-T reverse_scan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
+T reverse_scan(T& x, Func func, const mxx::comm& comm = mxx::comm()) {
     MPI_Comm rev;
     rev_comm(comm, rev);
     T result = scan(x, func, rev);
@@ -313,21 +384,21 @@ T reverse_scan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
  ************************/
 // useful for testing global conditions, such as termination conditions
 
-inline bool all_of(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
+inline bool all_of(bool x, const mxx::comm& comm = mxx::comm()) {
     int i = x ? 1 : 0;
     int result;
     MPI_Allreduce(&i, &result, 1, MPI_INT, MPI_LAND, comm);
     return result != 0;
 }
 
-inline bool any_of(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
+inline bool any_of(bool x, const mxx::comm& comm = mxx::comm()) {
     int i = x ? 1 : 0;
     int result;
     MPI_Allreduce(&i, &result, 1, MPI_INT, MPI_LOR, comm);
     return result != 0;
 }
 
-inline bool none_of(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
+inline bool none_of(bool x, const mxx::comm& comm = mxx::comm()) {
     int i = x ? 1 : 0;
     int result;
     MPI_Allreduce(&i, &result, 1, MPI_INT, MPI_LAND, comm);
---
+++ test/test_reductions.cpp
@@ -0,0 +1,137 @@
+/**
+ * @file    test_reductions.cpp
+ * @author  Patrick Flick <patrick.flick@gmail.com>
+ * @brief   GTest Unit Tests for mxx reductions
+ *
+ * Copyright (c) 2014 Georgia Institute of Technology. All Rights Reserved.
+ */
+
+#include <mpi.h>
+#include <gtest/gtest.h>
+
+#include <mxx/comm.hpp>
+#include <mxx/reduction.hpp>
+
+
+// test internal details of custom ops
+
+TEST(MxxImpl, CustomOp) {
+    // test C++ functors (postive tests)
+    {
+        std::plus<int> x;
+        mxx::custom_op<int> o(x);
+        EXPECT_EQ(MPI_INT, o.get_type());
+        EXPECT_EQ(MPI_SUM, o.get_op());
+    }
+    {
+        std::multiplies<double> x;
+        mxx::custom_op<double> o(x);
+        EXPECT_EQ(MPI_DOUBLE, o.get_type());
+        EXPECT_EQ(MPI_PROD, o.get_op());
+    }
+    {
+        std::bit_or<char> x;
+        mxx::custom_op<char> o(x);
+        EXPECT_EQ(MPI_CHAR, o.get_type());
+        EXPECT_EQ(MPI_BOR, o.get_op());
+    }
+    {
+        std::bit_xor<long> x;
+        mxx::custom_op<long> o(x);
+        EXPECT_EQ(MPI_LONG, o.get_type());
+        EXPECT_EQ(MPI_BXOR, o.get_op());
+    }
+    {
+        std::bit_and<unsigned char> x;
+        mxx::custom_op<unsigned char> o(x);
+        EXPECT_EQ(MPI_UNSIGNED_CHAR, o.get_type());
+        EXPECT_EQ(MPI_BAND, o.get_op());
+    }
+    {
+        std::logical_or<unsigned int> x;
+        mxx::custom_op<unsigned int> o(x);
+        EXPECT_EQ(MPI_UNSIGNED, o.get_type());
+        EXPECT_EQ(MPI_LOR, o.get_op());
+    }
+    {
+        std::logical_and<unsigned short> x;
+        mxx::custom_op<unsigned short> o(x);
+        EXPECT_EQ(MPI_UNSIGNED_SHORT, o.get_type());
+        EXPECT_EQ(MPI_LAND, o.get_op());
+    }
+    // test std::min and std::max
+    {
+        mxx::custom_op<float> o(std::min<float>);
+        EXPECT_EQ(MPI_FLOAT, o.get_type());
+        EXPECT_EQ(MPI_MIN, o.get_op());
+    }
+    {
+        mxx::custom_op<int> o(std::max<int>);
+        EXPECT_EQ(MPI_INT, o.get_type());
+        EXPECT_EQ(MPI_MAX, o.get_op());
+    }
+}
+
+template <typename T>
+struct mymax {
+    T operator()(const T x, T& y){
+        if (x > y)
+            return x;
+        else
+            return y;
+    }
+};
+
+int mymin(int x, int y) {
+    std::cout << "mymin called" << std::endl;
+    if (x < y) 
+        return x;
+    else
+        return y;
+}
+
+// scatter of size 1
+TEST(MxxReduce, ReduceOne) {
+    mxx::comm c = mxx::comm();
+
+    // test min
+    int x = -13*(c.size() - c.rank());
+    int y = mxx::reduce(x, c.size()/2, std::min<int>, c);
+    if (c.rank() == c.size()/2) {
+        ASSERT_EQ(-13*c.size(), y);
+    } else {
+        ASSERT_EQ(0, y);
+    }
+    // test sum
+    int z = mxx::reduce(3, 0, std::plus<int>(), c);
+    if (c.rank() == 0) {
+        ASSERT_EQ(3*c.size(), z);
+    } else {
+        ASSERT_EQ(0, z);
+    }
+    // test lambda op
+    int g = mxx::reduce(2, c.size()-1, [](int i, int j){return i+j;});
+    if (c.rank() == c.size()-1) {
+        ASSERT_EQ(2*c.size(), g);
+    } else {
+        ASSERT_EQ(0, g);
+    }
+    // test functor
+    float m = mxx::reduce(1.3333f*c.rank(), 0, mymax<float>(), c);
+    if (c.rank() == 0) {
+        ASSERT_EQ(1.3333f*(c.size()-1), m);
+    } else {
+        ASSERT_EQ(0, m);
+    }
+    // test own function pointer
+    int v = c.rank() + 1;
+    int u = mxx::reduce(v, 0, &mymin, c);
+    if (c.rank() == 0) {
+        ASSERT_EQ(1, u);
+    } else {
+        ASSERT_EQ(0, u);
+    }
+}
+
+
+// TODO: test for BIG MPI calls
