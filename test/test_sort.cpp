29a4dbf60b6f23ab75c33c2e0480d2a496a75da9
r4d2 <patrick.flick@gmail.com>
2015-09-28 15:55:58 -0400

    adding test for sort

--- test/test_sort.cpp
+++ test/test_sort.cpp
@@ -13,121 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <mpi.h>
+
+
+#include <gtest/gtest.h>
+#include <mxx/sort.hpp>
+#include <mxx/shift.hpp>
 
 #include <vector>
 #include <iostream>
 
-#include <mxx/partition.hpp>
-#include <mxx/sort.hpp>
-#include <mxx/timer.hpp>
+TEST(MxxSort, SampleSort1) {
+    mxx::comm c;
+    // simple sorting test with same number of elements for each processor
+    std::vector<int> vec(230);
+    srand(c.rank());
+    std::generate(vec.begin(), vec.end(), std::rand);
 
-typedef int element_t;
-
-class RandInput
-{
-private:
-    int mod;
-public:
-    RandInput(int seed = 0, int mod = 100) : mod(mod)
-    {
-        std::srand(seed);
-    }
-
-    element_t operator()()
-    {
-        return 1.13 * (std::rand() % mod);
-    }
-};
-
-void time_samplesort(std::size_t input_size, MPI_Comm comm)
-{
-    int p, rank;
-    MPI_Comm_size(comm, &p);
-    MPI_Comm_rank(comm, &rank);
-    mxx::timer t;
-    mxx::partition::block_decomposition<std::size_t> part(input_size, p, rank);
-
-    // generate local input
-    std::size_t local_size = part.local_size();
-    std::vector<element_t> local_els(local_size);
-    RandInput rand_input(rank, 4);
-    std::generate(local_els.begin(), local_els.end(), rand_input);
     // sort
-    MPI_Barrier(comm);
-    double start = t.elapsed();
-    mxx::sort(local_els.begin(), local_els.end(), std::less<element_t>(), comm);
-    MPI_Barrier(comm);
-    double duration = t.elapsed() - start;
-    if (rank == 0)
-        // print time taken in csv format
-        std::cout << p << ";" << input_size << ";" << duration << std::endl;
-    // check if input is sorted
-    if (!mxx::is_sorted(local_els.begin(), local_els.end(), std::less<element_t>(), comm))
-    {
-        std::cerr << "ERROR: Output is not sorted!" << std::endl;
-        exit(1);
-    }
-}
+    mxx::sort(vec.begin(), vec.end(), std::less<int>(), c);
 
-void print_usage()
-{
-    std::cerr << "Usage:  ./test_sort -n <input_size>" << std::endl;
-    exit(1);
-}
+    // should be locally sorted
+    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 
-void my_mpi_errorhandler(MPI_Comm* comm, int* errorcode, ...)
-{
-    // throw exception, enables gdb stack trace analysis
-    throw std::runtime_error("Shit: mpi fuckup");
-}
-
-int main(int argc, char *argv[])
-{
-    // set up MPI
-    MPI_Init(&argc, &argv);
-
-    // get communicator size and my rank
-    MPI_Comm comm = MPI_COMM_WORLD;
-    int p, rank;
-    MPI_Comm_size(comm, &p);
-    MPI_Comm_rank(comm, &rank);
-
-    // set custom error handler (for debugging with working stack-trace on gdb)
-    MPI_Errhandler errhandler;
-    MPI_Errhandler_create(&my_mpi_errorhandler, &errhandler);
-    MPI_Errhandler_set(comm, errhandler);
-
-
-    // parse input
-    std::size_t input_size = 0;
-    if (argc < 3)
-        print_usage();
-    if (argv[1][0] != '-')
-        print_usage();
-
-    switch(argv[1][1])
-    {
-        case 'n':
-            // set input_size
-            input_size = atol(argv[2]);
-            break;
-        case 'm':
-            // set input_size
-            input_size = static_cast<std::size_t>(p*atol(argv[2]));
-            break;
-        default:
-            print_usage();
+    // first element on each proc should be larger or equal to last one on
+    // previous processor
+    int prev = mxx::right_shift(vec.back(), c);
+    if (c.rank() > 0) {
+        ASSERT_TRUE(prev <= vec.front());
     }
 
-    if (input_size == 0)
-        print_usage();
+    // assume is sorted
+    bool sorted = mxx::is_sorted(vec.begin(), vec.end(), std::less<int>(), c);
+    ASSERT_EQ(true, sorted);
 
-    // run algorithm
-    MPI_Barrier(comm);
-    time_samplesort(input_size, comm);
-
-    // finalize MPI
-    MPI_Finalize();
-    return 0;
 }
+
