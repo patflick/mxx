3e44c54040aefd3e42c3b44178ce1fa9d31d8f9a
r4d2 <patrick.flick@gmail.com>
2015-10-02 13:27:57 -0400

    removing sudo

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -9,19 +9,19 @@ os=`uname`
 
 case "$os" in
     Linux)
-        sudo apt-get update -q
+        apt-get update -q
         case "$MPI_IMPL" in
             mpich2)
-                sudo apt-get install -q gfortran mpich2 libmpich2-3 libmpich2-dev
+                apt-get install -q gfortran mpich2 libmpich2-3 libmpich2-dev
                 ;;
             mpich|mpich3)
-                sudo apt-get install -q gfortran libcr0 default-jdk
+                apt-get install -q gfortran libcr0 default-jdk
                 wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.1/mpich_3.1-1ubuntu_amd64.deb
-                sudo dpkg -i ./mpich_3.1-1ubuntu_amd64.deb
+                dpkg -i ./mpich_3.1-1ubuntu_amd64.deb
                 rm -f ./mpich_3.1-1ubuntu_amd64.deb
                 ;;
             openmpi)
-                sudo apt-get install -q gfortran openmpi-bin openmpi-common libopenmpi-dev
+                apt-get install -q gfortran openmpi-bin openmpi-common libopenmpi-dev
                 ;;
             # TODO: openmpi 1.8 (MPI 3)
             *)
